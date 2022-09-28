505394bc5ef1604c57d1055806e279f5bea61cdc
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-09-29 01:02:44 +0300

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,10 @@ import { Renderlist } from './ListContact/ListContact';
 import { ContactList } from './ListContact/ListContact.styled';
 
 export const Phonebook = () => {
-  const [contact, setContact] = useState([]);
+  const [contact, setContact] = useState(() => {
+    return JSON.parse(localStorage.getItem('contact') || []);
+  });
   const [filter, setFilter] = useState('');
-  useEffect(() => {
-    setContact(prev =>
-      prev.concat(JSON.parse(localStorage.getItem('contact')) || [])
-    );
-  }, []);
 
   useEffect(() => {
     const pars = JSON.parse(localStorage.getItem('contact')) || [];
@@ -62,69 +59,3 @@ export const Phonebook = () => {
     </Box>
   );
 };
-
-// export class Phonebook extends Component {
-//   state = {
-//     contact: [],
-//     filter: '',
-//   };
-//   componentDidMount() {
-//     this.setState({
-//       contact: JSON.parse(localStorage.getItem('contact')) || [],
-//     });
-//   }
-
-//   componentDidUpdate(_, { contact }) {
-//     const pars = JSON.parse(localStorage.getItem('contact')) || [];
-//     if (!contact.map(item => item.name).includes(pars.map(item => item.name))) {
-//       localStorage.setItem('contact', JSON.stringify(this.state.contact));
-//     }
-//   }
-
-//   componentWillUnmount() {}
-//   hendleSubmit = ({ name, number }) => {
-//     const { contact } = this.state;
-//     const item = {
-//       id: nanoid(),
-//       name: name,
-//       number: number,
-//     };
-//     if (!contact.map(item => item.name).includes(name)) {
-//       this.setState(({ contact }) => ({
-//         contact: [item, ...contact],
-//       }));
-//     } else {
-//       Notiflix.Notify.warning(`${name} is already is contacts`);
-//     }
-//   };
-//   removeBtn = e => {
-//     const { contact } = this.state;
-//     this.setState({
-//       contact: contact.filter(item => item.id !== e.currentTarget.name),
-//     });
-//   };
-//   renderFilter = e => {
-//     this.setState({ filter: e.currentTarget.value.toLowerCase() });
-//   };
-
-//   renderContact = () => {
-//     const { contact, filter } = this.state;
-//     return contact.filter(item => item.name.toLowerCase().includes(filter));
-//   };
-
-//   render() {
-//     const { filter } = this.state;
-//     const contacts = this.renderContact();
-//     return (
-//       <Box display="flex" flexDirection="column" alignItems="center" p={8}>
-//         <h1>Phonebook</h1>
-//         <Formes onSubmit={this.hendleSubmit} />
-//         <h2>Contacts</h2>
-//         <Filter value={filter} onFilter={this.renderFilter} />
-//         <ContactList>
-//           <Renderlist onRemove={this.removeBtn} contact={contacts} />
-//         </ContactList>
-//       </Box>
-//     );
-//   }
-// }
